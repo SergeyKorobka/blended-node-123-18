46da3a3db0c5d3edcce46f1674c9fc78d9fe8161
mdarntl-code <bmartamartab@gmail.com>
2026-05-10 20:49:26 +0300

    add routes and controllers

--- src/server.js
+++ src/server.js
@@ -2,14 +2,15 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 
-// import { connectMongoDB } from './db/connectMongoDB.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFound } from './middleware/notFound.js';
 import { logger } from './middleware/logger.js';
+import routerProducts from './routes/productsRoutes.js';
 // import createHttpError from 'http-errors';
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT || 3030;
 
 // app.get('/error-test', () => {
 //   throw createHttpError(404, 'error');
@@ -18,11 +19,12 @@ const PORT = process.env.PORT ?? 3030;
 app.use(express.json());
 app.use(cors());
 app.use(logger);
+app.use(routerProducts);
 
 app.use(notFound);
 app.use(errorHandler);
 
-// await connectMongoDB();
+await connectMongoDB();
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
